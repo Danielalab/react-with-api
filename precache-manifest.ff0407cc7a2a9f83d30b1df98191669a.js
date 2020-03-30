ac7bf54cd25a830fc550a93fc8ce2a063f5519ce
Danielalab <olabolacrayola@hotmail.com>
2020-03-30 13:46:26 -0500

    Updates

---
+++ precache-manifest.ff0407cc7a2a9f83d30b1df98191669a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba46a3a5288dbe1155eb81921754c73e",
+    "url": "/react-with-api/index.html"
+  },
+  {
+    "revision": "a639e6c84be4352da2cf",
+    "url": "/react-with-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "049bded4b4bf70283f2f",
+    "url": "/react-with-api/static/js/2.dd263c71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-with-api/static/js/2.dd263c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a639e6c84be4352da2cf",
+    "url": "/react-with-api/static/js/main.155d96a6.chunk.js"
+  },
+  {
+    "revision": "69b92a2a8077ba8913e0",
+    "url": "/react-with-api/static/js/runtime-main.ef1575da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-with-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
